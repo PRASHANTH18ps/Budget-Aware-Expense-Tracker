53bb8df74ff6e30adbf9bb987596f6fa1084105a
PrashanthPS <82072919+PRASHANTH18ps@users.noreply.github.com>
2025-11-20 14:59:43 +0530

    Refactor MongoDB connection with dotenv support

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI;
-    await mongoose.connect(uri);
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected Successfully"));
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
